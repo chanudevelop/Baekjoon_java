e0663b1ac21a381790582bd853f19fe6a3ec5484
Kim Chanwoo <150686117+chanudevelop@users.noreply.github.com>
2025-12-02 14:23:32 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 192 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -7,20 +7,16 @@ import java.io.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 소문자인지 대문자인지에 따라 달라진다
-        String input = sc.nextLine();
-        int len = input.length();
-        String answer = "";
-        for(int i = 0; i<len; i++){
-            if(Character.isUpperCase(input.charAt(i))){ // 해당 문자가 대문자일 때
-                char temp = Character.toLowerCase(input.charAt(i));
-                answer += temp;
-            }else{ // 해당 문자가 소문자일 때
-                char temp = Character.toUpperCase(input.charAt(i));
-                answer += temp;
+        String str = sc.next();
+				String ans = "";
+        for(int i = 0 ; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if('A' <= ch && ch <= 'Z'){
+                ans += (char)('a' + ch - 'A');
+            }else{
+                ans += (char)('A' + ch - 'a');
             }
         }
-        System.out.println(answer);
-
+        System.out.println(ans);
     }
 }
